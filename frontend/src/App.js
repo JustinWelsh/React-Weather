1ec3814f846a579a4a1d866e003f1102cacdd751
Justin Welsh <justinwelsh33@gmail.com>
2022-11-04 19:49:36 -0500

    added daisyUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
