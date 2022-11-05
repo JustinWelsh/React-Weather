ecc74001bc620fd46a570e0a11d0721526eb982f
Justin Welsh <justinwelsh33@gmail.com>
2022-11-04 20:34:41 -0500

    clean up default react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { FaBeer } from 'react-icons/fa';
 
@@ -24,18 +23,6 @@ function App() {
 
         <h3> Lets go for a <FaBeer /></h3>
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
